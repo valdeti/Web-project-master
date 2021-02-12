b65e75cbe1da02abf5175f6a7bd7cdfa9754e505
valdeti <valdetshillova123@hotmail.com>
2021-02-12 15:12:12 +0100

    ri-rregullimi i sliderit

--- script.js
+++ script.js
@@ -34,5 +34,39 @@ function changeForm(param){
     qwe[0].classList.remove('form-style');
 }
 }
+                var divElements=document.getElementsByClassName('slider-content');
+                var sliderIndex = 0;
+                document.getElementsByClassName('slider')[0].addEventListener('click',function(event){
+                divElements[sliderIndex].classList.remove('active');
+                divElements[sliderIndex].classList.add('not-active');
+                
+                sliderIndex++;
+                if(sliderIndex==divElements.length) sliderIndex=0;
+                
+                //sliderIndex=(sliderIndex+1)%3;
+                
+                divElements[sliderIndex].classList.add('active');
+                divElements[sliderIndex].classList.remove('not-active');
+                })
+             
+                
+               
+                //-----------------------------------
+                // back button 
+                
+                document.getElementById('btn').addEventListener('click',function(event){
+                event.stopPropagation();
+                divElements[sliderIndex].classList.remove('active');
+                divElements[sliderIndex].classList.add('not-active');
+                
+                sliderIndex--;
+                if(sliderIndex<0) sliderIndex=divElements.length-1;
+                
+                //sliderIndex=(sliderIndex+1)%3;
+                
+                divElements[sliderIndex].classList.add('active');
+                divElements[sliderIndex].classList.remove('not-active');
+                })
+                
 
             
