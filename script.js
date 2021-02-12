0f47c94cc16c8866e1a413356c6283292d0f491a
valdeti <valdetshillova123@hotmail.com>
2021-02-12 14:55:08 +0100

    ndryshimet

--- script.js
+++ script.js
@@ -34,42 +34,5 @@ function changeForm(param){
     qwe[0].classList.remove('form-style');
 }
 }
-                var divElements=document.getElementsByClassName('slider-content');
-                var sliderIndex = 0;
-                document.getElementsByClassName('slider')[0].addEventListener('click',function(event){
-                divElements[sliderIndex].classList.remove('active');
-                divElements[sliderIndex].classList.add('not-active');
-                
-                sliderIndex++;
-                if(sliderIndex==divElements.length) sliderIndex=0;
-                
-                //sliderIndex=(sliderIndex+1)%3;
-                
-                divElements[sliderIndex].classList.add('active');
-                divElements[sliderIndex].classList.remove('not-active');
-                })
-<<<<<<< HEAD
-             
-                
-               
-=======
-                //-----------------------------------
-                // back button 
-                
-                document.getElementById('btn').addEventListener('click',function(event){
-                event.stopPropagation();
-                divElements[sliderIndex].classList.remove('active');
-                divElements[sliderIndex].classList.add('not-active');
-                
-                sliderIndex--;
-                if(sliderIndex<0) sliderIndex=divElements.length-1;
-                
-                //sliderIndex=(sliderIndex+1)%3;
-                
-                divElements[sliderIndex].classList.add('active');
-                divElements[sliderIndex].classList.remove('not-active');
-                })
->>>>>>> 43d6ae6d6f58bb429739ea519fef7965fe464dfe
-                
 
             
